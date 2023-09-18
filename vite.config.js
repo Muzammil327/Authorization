9458684062c7feb77e736986188bdd5232097247
Muzammil327 <mmuzammiloff327@gmail>
2023-09-18 08:27:14 +0500

    updated 404 page error

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    base: '/', // Ensure this is set to '/'
+    // ... other server configuration options
+  },
 })
